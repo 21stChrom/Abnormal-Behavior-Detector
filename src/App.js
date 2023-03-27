0081ba64f803735e29565b334056da148fd687a5
21stChrom <81393334+21stChrom@users.noreply.github.com>
2023-03-27 07:24:37 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
-import "./style.css";
-
-export default function App() {
-  return (
-    <div>
-      </div>
-  );
-}
+modify this code to detect seizures
+code for an iOS app that can detect and monitor all the 13 sensors on a device:
+```swift
+import CoreMotion
+import CoreLocation
+class SensorManager: NSObject, CLLocationManagerDelegate {
+    let motionManager = CMMotionManager()
+        let locationManager = CLLocationManager()
+            func startSensors() {
+                    startAccelerometer()
+                            startGyroscope()
+                                    startMagnetometer()
+                                            startDeviceMotion()
+                                                    startPedometer()
+                                                            startAltimeter()
+                                                                    startBarometer()
+                                                                            startProximitySensor()
+                                                                                    startLightSensor()
+                                                                                            startMicrophone()
+                                                                                                    startCamera()
+                                                                                                            startGPS()
+                                                                                                                    startCompass()
+                                                                                                                        }
+                                                                                                                            
+                                                                                                                                func startAccelerometer() {
+                                                                                                                                        if motionManager.isAccelerometerAvailable {
+                                                                                                                                                    motionManager.accelerometerUpdateInterval = 1 / 60
+                                                                                                                                                                motionManager.startAccelerometerUpdates(to: .main) { (data, error) in
+                                                                                                                                                                                guard let accelerometerData = data else
